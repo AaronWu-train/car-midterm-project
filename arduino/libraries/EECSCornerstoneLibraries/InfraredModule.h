ab4bf497d462e042d57b481fcb8405a92eae5ded
ShengYuCheng <sean.sheng.yu.cheng@gmail.com>
2025-03-23 00:03:35 +0800

    feat: created motor controller classes and refined struct/class of IR modules

--- arduino/libraries/EECSCornerstoneLibraries/InfraredModule.h
+++ arduino/libraries/EECSCornerstoneLibraries/InfraredModule.h
@@ -1,21 +1,21 @@
 #ifndef _INFRARED_MODULE_H_
 #define _INFRARED_MODULE_H_
 
-typedef class DigitalInfraredModule {
-    public:
-        int m_pin_number;
-        DigitalInfraredModule(void);
-        DigitalInfraredModule(int pin_number);
-        bool value(void);
-} DigitalIF;
+struct DigitalInfraredModule {
+    int m_pin_number;
+    DigitalInfraredModule(void);
+    DigitalInfraredModule(int pin_number);
+    void init(int pin_number);
+    bool detect(void);
+};
 
-typedef class AnalogInfraredModule {
-    public:
-        int m_pin_number;
-        AnalogInfraredModule(void);
-        AnalogInfraredModule(int pin_number);
-        int value(void);
-} AnalogIF;
+struct AnalogInfraredModule {
+    int m_pin_number;
+    AnalogInfraredModule(void);
+    AnalogInfraredModule(int pin_number);
+    void init(int pin_number);
+    int detect(void);
+};
 
 class DigitalInfraredArray {
     public:
@@ -23,6 +23,7 @@ class DigitalInfraredArray {
         DigitalInfraredArray(int infrared_module_amount);
         DigitalInfraredArray(int infrared_module_amount, int *infrared_module_pins);
         int detect(void); // (infared_module_amount) bit integer, every bit represents the detection result of a IR module
+        int size(void);
     private:
         int m_infrared_module_amount;
         DigitalInfraredModule *m_infrared_modules;
@@ -34,6 +35,7 @@ class AnalogInfraredArray {
         AnalogInfraredArray(int infrared_module_amount);
         AnalogInfraredArray(int infrared_module_amount, int *infrared_module_pins);
         int* detect(void); // integer array of size infared_module_amount, every number represents the detection result of a IR module
+        int size(void);
     private:
         int m_infrared_module_amount;
         AnalogInfraredArray *m_infrared_modules;
--- arduino/libraries/EECSCornerstoneLibraries/MotorController.h
+++ arduino/libraries/EECSCornerstoneLibraries/MotorController.h
@@ -1,6 +1,37 @@
 #ifndef _MOTOR_CONTROLLER_H_
 #define _MOTOR_CONTROLLER_H_
 
+struct PulseWidthModulationController {
+    int m_pin_number, m_current_speed;
+    PulseWidthModulationController(void);
+    PulseWidthModulationController(int pin_number);
+    void init(int pin_number);
+    void output(int value); // 0 <= value <= 255
+    int currentSpeed(void); // used to check if current speed is same as previous in order not to repeatedly output the same value
+};
 
+struct BridgeCircuitDirectionController {
+    int m_first_pin_number, m_second_pin_number;
+    bool m_current_direction;
+    BridgeCircuitDirectionController(void);
+    BridgeCircuitDirectionController(int first_pin_number, int second_pin_number);
+    void init(int first_pin_number, int second_pin_number);
+    void output(bool direction); // 0 for forward, 1 for backward
+    bool currentDirection(void); // used to check if current direction is same as previous in order not to repeatedly output the same value
+};
+
+class MotorController {
+    private:
+        PulseWidthModulationController m_left_pwm_controller, m_right_pwm_controller;
+        BridgeCircuitDirectionController m_left_bridge_controller, m_right_bridge_controller;
+    public:
+        MotorController();
+        MotorController(
+            int left_pwm_pin_number, int right_pwm_pin_number,
+            int left_first_bridge_pin_number, int left_second_bridge_pin_number, 
+            int right_first_bridge_pin_number, int right_second_bridge_pin_number
+        );
+        void setSpeed(int left_motor_speed, int right_motor_speed); // -255 <= left/right motor speed <= 255
+};
 
 #endif
