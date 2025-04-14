08412e2f5aec51e309db28bf407c180a14f0521a
AaronWu-train <aaronwu0218@gmail.com>
2025-04-14 16:23:43 +0800

    minor change in style

--- BFS/BFS.cpp
+++ BFS/BFS.cpp
@@ -17,10 +17,9 @@ vector<vector<Edge>> buildGraph(vector<vector<int>>);
 int getDir(int, int, int);
 vector<vector<int>> table;
 
-int main(){
+int main() {
     vector<vector<string>> data = readCsv("maze.csv");
-    for (size_t i=1; i<data.size(); i++)
-    {
+    for (size_t i=1; i<data.size(); i++) {
         vector<int> row = StrToVec(data[i]);
         table.push_back(row);
     }
@@ -38,7 +37,7 @@ int main(){
     return 0;
 }
 
-void Dijkstra(vector<vector<Edge>> graph, int start, int end){
+void Dijkstra(vector<vector<Edge>> graph, int start, int end) {
     int INF = 10000;
     vector<int> dist(100, INF);
     vector<bool> visited(100, false);
@@ -46,21 +45,17 @@ void Dijkstra(vector<vector<Edge>> graph, int start, int end){
     
     dist[start] = 0;
 
-    for (int i=0; i<graph.size(); i++)
-    {
+    for (int i = 0; i < graph.size(); i++) {
         int u = -1;
-        for (int j=0; j<graph.size(); j++)
-        {
+        for (int j = 0; j < graph.size(); j++){
             if (!visited[j] && dist[j]<INF && (u==-1 || dist[j]<dist[u]))
                 u = j;
         }
         if (u==end) break;
         visited[u] = true;
         
-        for (Edge& e: graph[u])
-        {
-            if (dist[e.to] > dist[u] + e.cost)
-            {
+        for (Edge& e: graph[u]) {
+            if (dist[e.to] > dist[u] + e.cost) {
                 dist[e.to] = dist[u] + e.cost;
                 prev[e.to] = u;
             }
@@ -71,15 +66,13 @@ void Dijkstra(vector<vector<Edge>> graph, int start, int end){
     stack<int> path;
     int node = end;
     path.push(node);
-    while (node!=start)
-    {
+    while (node != start) {
         path.push(prev[node]);
         node = prev[node];
     }
 
     int currOrient = -1;
-    while (path.size()>1)
-    {
+    while (path.size()>1) {
         int curr = path.top();
         path.pop();
         currOrient = getDir(curr, path.top(), currOrient);        
@@ -87,7 +80,7 @@ void Dijkstra(vector<vector<Edge>> graph, int start, int end){
 
 }
 
-vector<int> StrToVec(const vector<string>& rowStr){
+vector<int> StrToVec(const vector<string>& rowStr) {
     vector<int> row;
     for (size_t i = 0; i < rowStr.size(); i++) {
         string cell = rowStr[i];
@@ -106,35 +99,30 @@ vector<int> StrToVec(const vector<string>& rowStr){
 
 vector<vector<Edge>> buildGraph(vector<vector<int>> table) {
     vector<vector<Edge>> graph(100);
-    for(int i=0; i<table.size(); i++)
-    {
+    for (int i = 0; i < table.size(); i++) {
         int node = table[i][0];
         vector<int> dir = {table[i][1], table[i][2], table[i][3], table[i][4]};
         vector<int> distance = {table[i][5], table[i][6], table[i][7], table[i][8]};
-        for (int j=0; j<4; j++)
-        {
-            if (dir[j]!=-1 && distance[j]!=-1)
+        for (int j = 0; j < 4; j++){
+            if (dir[j] != -1 && distance[j] != -1)
                 graph[node].push_back({dir[j], distance[j]});
         }
     }
     return graph;
 }
 
-int getDir(int from, int to, int currOrient){
+int getDir(int from, int to, int currOrient) {
     //orient: 0:N, 1:E, 2:S, 3:W
     int orient;
     if (table[from-1][1] == to) orient = 0;
     else if (table[from-1][2] == to) orient = 2;
     else if (table[from-1][3] == to) orient = 3;
     else if (table[from-1][4] == to) orient = 1;
-    if (currOrient == -1)
-    {
+    if (currOrient == -1) {
         cout << 'f';
         return orient;
-    }
-    else
-    {
-        int relative = ((orient-currOrient)+4)%4;
+    } else {
+        int relative = ((orient - currOrient) + 4) % 4;
         if (relative==0) cout << 'f';
         else if (relative==1) cout << 'r';
         else if (relative==2) cout << 'b';
